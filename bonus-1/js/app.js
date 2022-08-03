d926e27f7bb9f36879db9223d244b1b586397a94
MaryChiodelli <marychiodelli97@gmail.com>
2022-08-03 15:43:04 +0200

    add bonus-1 folder

---
+++ bonus-1/js/app.js
@@ -0,0 +1,43 @@
+const bodyEl = document.querySelector('body');
+
+const team = [
+    {
+        name: 'Wayne Barnett',
+        role: 'Founder & CEO',
+        image: 'wayne-barnett-founder-ceo.jpg'
+    },
+    {
+        name: 'Angela Caroll',
+        role: 'Chief Editor',
+        image: 'angela-caroll-chief-editor.jpg'
+    },
+    {
+        name: 'Walter Gordon',
+        role: 'Office Manager',
+        image: 'walter-gordon-office-manager.jpg'
+    },
+    {
+        name: 'Angela Lopez',
+        role: 'Social Media Manager',
+        image: 'angela-lopez-social-media-manager.jpg'
+    },
+    {
+        name: 'Scott Estrada',
+        role: 'Developer',
+        image: 'scott-estrada-developer.jpg'
+    },
+    {
+        name: 'Barbara Ramos',
+        role: 'Graphic Designer',
+        image: 'barbara-ramos-graphic-designer.jpg'
+    }
+];
+
+for (let i = 0; i < team.length; i++) {
+    const teamMember = team[i];
+    const name = teamMember.name;
+    const role = teamMember.role;
+    const image = teamMember.image;
+
+    bodyEl.innerHTML += `<p>${name} ${role} ${image}</p>`;
+}
