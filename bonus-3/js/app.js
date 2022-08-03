c0f5e3e0942131b55da508754c50eca47afb94af
MaryChiodelli <marychiodelli97@gmail.com>
2022-08-03 16:34:43 +0200

    add bonus-3 folder

---
+++ bonus-3/js/app.js
@@ -0,0 +1,54 @@
+const bodyEl = document.querySelector('body');
+
+const team = [
+    {
+        name: 'Wayne Barnett',
+        role: 'Founder & CEO',
+        image: 'wayne-barnett-founder-ceo.jpg'
+    },
+    {
+        name: 'Angela Caroll',
+        role: 'Chief Editor',
+        image: 'angela-caroll-chief-editor.jpg'
+    },
+    {
+        name: 'Walter Gordon',
+        role: 'Office Manager',
+        image: 'walter-gordon-office-manager.jpg'
+    },
+    {
+        name: 'Angela Lopez',
+        role: 'Social Media Manager',
+        image: 'angela-lopez-social-media-manager.jpg'
+    },
+    {
+        name: 'Scott Estrada',
+        role: 'Developer',
+        image: 'scott-estrada-developer.jpg'
+    },
+    {
+        name: 'Barbara Ramos',
+        role: 'Graphic Designer',
+        image: 'barbara-ramos-graphic-designer.jpg'
+    }
+];
+
+for (let i = 0; i < team.length; i++) {
+    const teamMember = team[i];
+    const name = teamMember.name;
+    const role = teamMember.role;
+    const image = teamMember.image;
+    
+    bodyEl.innerHTML += createCard(name, role, image);
+}
+
+function createCard(name, role, image) {
+    const card = `
+    <div class="card">
+        <img src="./img/${image}" alt="foto di ${name}">
+        <h2>${name}</h2>
+        <p>${role}</p>
+    </div>
+    `;
+    return card;
+}
